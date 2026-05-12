55326244b3db6925988eb057605a03d7a6aa6d10
baralprativa27 <baralprativa27@gmail.com>
2026-05-12 10:32:57 -0400

    Update 06_claims_summary.sql

--- sql_scripts/06_claims_summary.sql
+++ sql_scripts/06_claims_summary.sql
@@ -23,24 +23,3 @@ SELECT
     ) AS rebate_percentage
 
 FROM claims;
-
-
-
-
-
-
-
-SELECT
-    COUNT(claim_id) AS total_claims,
-    SUM(total_cost) AS total_cost,
-    SUM(rebate_amount) AS total_rebate,
-    
-    SUM(total_cost) - SUM(rebate_amount)
-        AS net_cost,
-        
-    ROUND(
-        SUM(rebate_amount) /
-        SUM(total_cost) * 100,
-        2
-    ) AS rebate_percentage
-FROM claims;
