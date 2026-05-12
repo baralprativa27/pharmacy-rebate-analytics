804111009d86e83dbb126cbd2857e467871f7f2b
baralprativa27 <baralprativa27@gmail.com>
2026-05-11 23:25:49 -0400

    Create 07_high_cost_drugs.sql

---
+++ sql_scripts/07_high_cost_drugs.sql
@@ -0,0 +1,31 @@
+-- ============================================
+-- Project: Pharmacy Rebate Analytics Dashboard
+-- File: 07_high_cost_drugs.sql
+-- Purpose:
+-- Identify high-cost drugs contributing
+-- significantly to pharmacy spending.
+-- ============================================
+
+SELECT
+    d.drug_name,
+    d.therapeutic_class,
+
+    COUNT(c.claim_id) AS total_claims,
+
+    SUM(c.total_cost) AS total_cost,
+
+    ROUND(
+        AVG(c.total_cost),
+        2
+    ) AS average_claim_cost
+
+FROM claims c
+
+JOIN drugs d
+    ON c.drug_id = d.drug_id
+
+GROUP BY
+    d.drug_name,
+    d.therapeutic_class
+
+ORDER BY total_cost DESC;
