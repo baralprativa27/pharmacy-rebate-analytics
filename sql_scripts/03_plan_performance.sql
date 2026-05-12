44ca8682989779cb7ab77fbaa7648e814ae6fb62
baralprativa27 <baralprativa27@gmail.com>
2026-05-11 23:07:01 -0400

    Create 03_plan_performance.sql

---
+++ sql_scripts/03_plan_performance.sql
@@ -0,0 +1,19 @@
+SELECT
+    p.plan_name,
+    p.plan_type,
+    COUNT(c.claim) AS total_claims,
+    SUM(c.total_cost) AS total_cost,
+    SUM(c.rebate_amount) AS total_rebate,
+    
+    ROUND(
+        SUM(c.rebate_amount) /
+        SUM(c.total_cost) * 100,
+        2
+    ) AS rebate_percentage
+FROM claims c
+JOIN plans p
+    ON c.plan_id = p.plan_id
+GROUP BY
+    p.plan_name,
+    p.plan_type
+ORDER BY total_rebate DESC;
